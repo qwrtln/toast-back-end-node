ce9ceb070768f1e82c340e8f45d8996dc8edc90d
Marcin Szewczyk <marcin91.szewczyk@gmail.com>
2017-11-04 22:23:44 +0100

    Add sending mail on guest using form

--- web-services/guests-ws.js
+++ web-services/guests-ws.js
@@ -1,15 +1,21 @@
-var express = require('express');
-var fs = require('fs');
-var app = module.exports = express.Router();
+const express = require('express');
+const fs = require('fs');
+const app = module.exports = express.Router();
+const nodemailer = require('nodemailer');
+
+const config = require("../config.json");
+const mailConfig = config.mailConfig;
 
 app.post('/api/submit', function(req,res) {
 
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
+  sendMailWithGuestData(req.body.email);
+
   const desc = req.body.email + ", " + new Date().toLocaleString() + ",\n";
 
-  fs.appendFile("goscie", desc, function(err) {
+  fs.appendFile(config.guestsFile, desc, function(err) {
     if(err) {
       return console.log(err);
     }
@@ -33,3 +39,32 @@ app.get('/api/guests', function(req, res) {
   });
 
 });
+
+function sendMailWithGuestData(guestEmail) {
+  let transporter = nodemailer.createTransport({
+    host: mailConfig.server,
+    port: mailConfig.port,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: mailConfig.user,
+      pass: mailConfig.pass
+    }
+  });
+
+  let mailOptions = {
+    from: mailConfig.sender,
+    to: mailConfig.receiver,
+    subject: 'Nowy kontakt na stronie',
+    html: '<h2>Hurra, hurra!</h2><p>Ludzie nas kochają i zostawiają nam swoje maile!</p> ' +
+    '<p>Wiedz, że jakaś osoba użyła formularza na stronie <u>toastmastersstudents.pl</u>, aby pozostawić nam kontakt do siebie. Nie rozczarujmy go, wyślijmy mu jakąś ładną wiadomość powitalną :-)' +
+    '<p><b>Mail: </b> ' + guestEmail + '</p>' +
+    '<p><b>Czas:</b> ' + new Date().toLocaleString() + '</p>'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+  });
+}
